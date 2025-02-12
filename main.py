18809e79a41ec49b03cadbdf17fb85c59f7fbd41
ender-orwell <enderOrwell@outlook.com>
2025-02-12 19:15:04 +0100

    adding website

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import random
 import string
 
 def get_user_input():
-    password_size = int(input("Enter the sixze of the password (max 30) "))
+    password_size = int(input("Enter the size of the password (max 30) "))
     if password_size < 1 and password_size > 30:
         return -1
-    mix_upper_lower = input("Do you need to mix upper and lower case? (Yes / No)").strip().lower() in ('yes', 'y')
-    include_numbers = input("Do you need that it has numbers? (Yes / No)").strip().lower() in ('yes', 'y')
-    include_special_char = input("Do you need that it has special characters? (Yes / No)").strip().lower() in ('yes', 'y')
+    mix_upper_lower = input("Do you need to mix upper and lower case? (Yes / No) ").strip().lower() in ('yes', 'y')
+    include_numbers = input("Do you need that it has numbers? (Yes / No) ").strip().lower() in ('yes', 'y')
+    include_special_char = input("Do you need that it has special characters? (Yes / No) ").strip().lower() in ('yes', 'y')
 
     return password_size, mix_upper_lower, include_numbers, include_special_char
 
@@ -24,12 +24,13 @@ def generate_password(password_size, mix_upper_lower, include_numbers, include_s
     return password
 
 def main():
+    website = input("Where do you want to use the password? ")
     password_size, mix_upper_lower, include_numbers, include_special_char = get_user_input()
     if password_size == -1:
         print("Error: Password size must be between 1 and 30")
     else:
         password = generate_password(password_size, mix_upper_lower, include_numbers, include_special_char)
-        print(f"Generated password: {password}")
+        print(f"The new generated password for {website} is {password}")
 
 if __name__ == "__main__":
     main()
